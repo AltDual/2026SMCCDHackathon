309d19e1367163e0a2dcb60ecb2435e974e0f067
TheDarkShadow-dev <titanbstar@gmail.com>
2026-04-21 18:04:20 -0700

    dungeon generation finished

---
+++ scripts/rooms/room-script.gd
@@ -0,0 +1,12 @@
+extends RefCounted
+class_name RoomData
+
+var grid_pos: Vector2i
+var exists: bool = false
+
+var door_up: bool = false
+var door_down: bool = false
+var door_left: bool = false
+var door_right: bool = false
+
+var room_type: String = "normal"
