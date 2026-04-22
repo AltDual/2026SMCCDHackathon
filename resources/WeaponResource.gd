07e3fa165a2aec5fb3df7f583d73de5c8b01068c
Taiki Shinokawa <ts3078541@gmail.com>
2026-04-22 10:02:48 -0700

    Fixed weapon

--- resources/WeaponResource.gd
+++ resources/WeaponResource.gd
@@ -3,11 +3,13 @@ extends Resource
 
 @export var weapon_name: String = "Pistol"
 @export var damage: int = 10
-@export var fire_rate: float = 0.3
+@export var fire_rate: float = 0.05
 @export var bullet_speed: float = 300.0
 @export var bullet_range: float = 800.0
-@export var mag_ammo: int = 12
-@export var reserve_ammo: int = 48
-@export var reload_time: float = 1.5
+
+
+@export var pattern: String = "circle"  # "single", "spread", "circle"
+@export var pellet_count: int = 5
+@export var spread_angle: float = 30.0
+
 @export var is_automatic: bool = false
-@export var pellet_count: int = 1  
--- scripts/Gun/gun.gd
+++ scripts/Gun/gun.gd
@@ -6,16 +6,7 @@ const BULLET = preload("res://scenes/bullet.tscn")
 @export var offset := 10.0
 @onready var muzzle: Marker2D = $Marker2D
 
-var current_ammo: int
-var reserve_ammo: int
 var can_fire: bool = true
-var is_reloading: bool = false
-
-func _ready() -> void:
-	if weapon_data:
-		current_ammo = weapon_data.mag_ammo
-		reserve_ammo = weapon_data.reserve_ammo
-		SignalBus.ammo_changed.emit(current_ammo)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -29,44 +20,52 @@ func _process(delta: float) -> void:
 	
 	var fire_input = Input.is_action_just_pressed("shoot") if not weapon_data.is_automatic else Input.is_action_pressed("shoot")
 	
-	if fire_input and can_fire and not is_reloading:
-		if current_ammo > 0:
-			fire()
-		else:
-			reload()
+	if fire_input and can_fire:
+		fire()
 	
-	if Input.is_action_just_pressed("reload") and not is_reloading:
-		reload()
-	#if Input.is_action_just_pressed("shoot"):
-	#	var bullet_instance = BULLET.instantiate()
-	#	get_tree().root.add_child(bullet_instance)
-	#	bullet_instance.global_position = muzzle.global_position
-	#	bullet_instance.rotation = rotation
+
 
 func fire() -> void:
 	can_fire = false
-	current_ammo -= 1
-	SignalBus.ammo_changed.emit(current_ammo, reserve_ammo)
-
-	for i in weapon_data.pellet_count:
-		var bullet = BULLET.instantiate()
-		get_tree().root.add_child(bullet)
-		bullet.global_position = muzzle.global_position
-		bullet.rotation = rotation
-	#	bullet.setup(weapon_data.damage, weapon_data.bullet_speed, weapon_data.bullet_range)
+	
+	match weapon_data.pattern:
+		"single":
+			fire_single()
+		"spread":
+			fire_spread()
+		"circle":
+			fire_circle()
 
 	await get_tree().create_timer(weapon_data.fire_rate).timeout
 	can_fire = true
 
-func reload() -> void:
-	if reserve_ammo <= 0 or current_ammo == weapon_data.mag_ammo:
-		return
-	is_reloading = true
-	# TODO: play reload animation here
-	await get_tree().create_timer(weapon_data.reload_time).timeout
-	var needed = weapon_data.mag_ammo - current_ammo
-	var given = min(needed, reserve_ammo)
-	current_ammo += given
-	reserve_ammo -= given
-	is_reloading = false
-	SignalBus.ammo_changed.emit(current_ammo, reserve_ammo)
+func fire_single() -> void:
+	spawn_bullet(rotation)
+
+
+func fire_spread() -> void:
+	var count := weapon_data.pellet_count
+	var spread := deg_to_rad(weapon_data.spread_angle)
+
+	for i in range(count):
+		var t := 0.0 if count == 1 else float(i) / (count - 1)
+		var angle_offset: float = lerp(-spread / 2.0, spread / 2.0, t)
+		spawn_bullet(rotation + angle_offset)
+
+
+func fire_circle() -> void:
+	var count := weapon_data.pellet_count
+
+	for i in range(count):
+		var angle := (TAU / count) * i
+		spawn_bullet(angle)
+
+func spawn_bullet(angle: float) -> void:
+	var bullet = BULLET.instantiate()
+	get_tree().root.add_child(bullet)
+
+	bullet.global_position = muzzle.global_position
+	bullet.rotation = angle
+
+	# Optional if you implement it
+	# bullet.setup(weapon_data.damage, weapon_data.bullet_speed, weapon_data.bullet_range)
